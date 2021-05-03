d1742b108943aaf3e12b9b09c3331e036fead78f
txnguyen292 <tnguyen2921@DESKTOP-MGUPQGF.localdomain>
2021-05-02 23:28:45 -0400

    test_HAND. added get_high_cards function

--- src/tests/test_Hands.py
+++ src/tests/test_Hands.py
@@ -2,8 +2,9 @@
 import sys
 import numpy as np
 import pytest
+from collections import defaultdict
 
-from typing import List, Literal
+from typing import List, Literal, Set, DefaultDict
 from config import CONFIG
 sys.path.insert(0, str(CONFIG.src))
 from Hands import Hands
@@ -51,7 +52,7 @@ def get_same_suits(same_suits: List[Card_Deck.Card], cards: Card_Deck)-> List[Ca
     Returns:
         List[Card_Deck.Card]: list that contain at least 5 cards of the same suit
     """
-    first_suit = same_suits[0].suit
+    first_suit: str = same_suits[0].suit
     while len(same_suits) < 5:
         for idx, card in enumerate(cards.cards):
             if card.suit == first_suit:
@@ -59,18 +60,6 @@ def get_same_suits(same_suits: List[Card_Deck.Card], cards: Card_Deck)-> List[Ca
     for _ in range(2):
         same_suits.append(cards.deal())
     return same_suits
-
-def get_four_of_a_kind(four_of_a_kind: List[Card_Deck.Card], cards: Card_Deck) -> List[Card_Deck.Card]:
-    """Returns a list that contains four of a kind
-
-    Args:
-        four_of_a_kind (List[Card_Deck.Card]): [description]
-        cards (Card_Deck): [description]
-
-    Returns:
-        List[Card_Deck.Card]: [description]
-    """
-    pass
 
 def get_same_value(first_card: List[Card_Deck.Card], cards: Card_Deck, type_of_pair: Literal["two", "three", "four"]) -> List[Card_Deck.Card]:
     """Returns a list that contains fives which has number of type pairs
@@ -83,35 +72,65 @@ def get_same_value(first_card: List[Card_Deck.Card], cards: Card_Deck, type_of_p
     Returns:
         List[Card_Deck.Card]: List of 7 cards with type of pair
     """
-    valToInt = {"two": 2, "three": 3, "four": 4}
-    first_value = first_card[0].value
-    idx = 0
+    valToInt: Dict[str, int] = {"two": 2, "three": 3, "four": 4}
+    first_value: int = first_card[0].value
+    idx: int = 0
     while len(first_card) < valToInt[type_of_pair] and idx < len(cards.cards):
         if cards.cards[idx].value == first_value:
             first_card.append(cards.cards[idx])
         idx += 1
     return first_card
 
-def get_three_of_a_kind(three_of_a_kinds: List[Card_Deck.Card], cards: Card_Deck) -> List[Card_Deck.Card]:
-    """Return a a list that contains five cards of three of a kind
+def get_high_card(first_card: List[Card_Deck.Card], cards: Card_Deck) -> List[Card_Deck.Card]:
+    """Return a list that contains only high cards
 
     Args:
-        three_of_a_kinds (List[Card_Deck.Card]): List with first card
+        first_card (List[Card_Deck.Card]): list with first card
         cards (Card_Deck): card deck
 
     Returns:
-        List[Card_Deck.Card]: list that contains three of a kind hand
+        List[Card_Deck.Card]: high card hand
     """
-    first_value = three_of_a_kinds[0].value
+    first_value: int = first_card[0].value
+    first_suit: str = first_card[0].suit
+    values: Set = set()
+    suits: DefaultDict[str, int] = defaultdict(int)
+    straight_upper = 1
+    straight_lower = 1
     idx = 0
-    while len(three_of_a_kinds) < 3 and idx < len(cards):
-        if cards[idx].value == first_value:
-            three_of_a_kinds.append(cards[idx])
+    while len(first_card) < 7 and idx < len(cards.cards):
+        current_card = cards.cards[idx]
+        if current_card.value in values: # if a pair, skip
+            idx += 1
+            continue
+        if current_card.suit in suits and suits[current_card.suit] >= 4: # if a flush, skip
+            idx += 1
+            continue
+        else: 
+        # Check if hand contains a straight
+        if first_value < 5:
+            if straight_upper >= 4:
+                idx += 1
+                continue
+            if current_card.value > first_card[-1].value: #TODO: should compare with the maximum value of first_card
+                straight_upper += 1
+        else:
+            if straight_lower >= 4:
+                idx += 1
+                continue
+            if current_card.value < first_card[0].value: #TODO: Compare with the minimum value of first_card
+                straight_lower += 1
+            if straight_upper >= 4:
+                idx += 1
+                continue
+            if current_card.value > first_card[-1].value:
+            
+        values.add(current_card.value)
+        suits[current_card.suit] += 1
+        first_card.append(current_card)
         idx += 1
-    while len(three_of_a_kinds) < 7:
-        three_of_a_kinds.append(card.deal())
-    return three_of_a_kinds
-        
+        #TODO: Finish generating just high cards
+    return first_card
     
 #======================================== END ===============================================
 
@@ -165,6 +184,8 @@ def test_is_highcard(card_deck, hand):
     five_of_same_suits = [card for card in card_deck.cards if card.suit == random_card.suit]
     straights = [random_card]
     get_straight(straights, card_deck)
+    high_cards = get_high_card([random_card], card_deck)
+    assert hand._is_highcard, f"Check if {high_cards} is a high card hand"
     assert not hand._is_highcard(five_of_same_suits), "Flush is not high card!"
     assert not hand._is_highcard(straights), "Straight is not high cards!"
     # assert hand._is_highcard()
@@ -176,7 +197,6 @@ def test_is_pair(card_deck, hand):
     one_pair = get_same_value([random_card], card_deck, type_of_pair="two")
     assert hand._is_pair, f"Checking if {one_pair} contains a pair"
 
-
 def test_is_three_of_a_kind(card_deck, hand):
     card_deck.shuffle()
     random_ind = np.random.randint(0, 52, 1)[0]
@@ -185,7 +205,16 @@ def test_is_three_of_a_kind(card_deck, hand):
     assert hand._is_three_of_a_kind(three_of_a_kind), f"Checking three of a kind of {three_of_a_kind}"
 
 def test_is_four_of_a_kind(card_deck, hand):
-    pass
+    card_deck.shuffle()
+    random_ind = np.random.randint(0, 52, 1)[0]
+    random_card = card_deck.cards[random_ind]
+    four_of_a_kind = get_same_value([random_card], card_deck, "four")
+    flush = get_same_suits([random_card], card_deck)
+    straight = get_straight([random_card], card_deck)
+    high_card = get_high_card([random_card], card_deck)
+    assert hand._is_four_of_a_kind(four_of_a_kind), f"Checking four of a kind of {four_of_a_kind}"
+    assert not hand._is_four_of_a_kind(flush), f"A flush can't be a four of a kind"
+    assert not hand._is_four_of_a_kind(straight), f"A straight can't be a four of a kind"
 
     
 if __name__ == "__main__":
